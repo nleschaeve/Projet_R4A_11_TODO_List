430bdaacfb82b6ef12e2354c686b4bf3f8cbf9cd
LESCHAEVE--LEMAIRE Nathaniel <nathaniel.leschaevelemaire@etu.unilim.fr>
2026-02-12 09:57:29 +0100

    Updates app theme with new color scheme

--- app/src/main/java/com/example/todolist/ui/theme/Color.kt
+++ app/src/main/java/com/example/todolist/ui/theme/Color.kt
@@ -2,10 +2,17 @@ package com.example.todolist.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val HeaderDarkBackground = Color(0xFF222222)
+val HeaderLightBackground = Color(0xFFE0E0E0)
+val MainDarkBackground = Color(0xFF333333)
+val MainLightBackground = Color(0xFFF5F5F5)
+val TaskDarkBackground = Color(0xFF202020)
+val TaskLightBackground = Color(0xFFE2E2E2)
+val InputDarkBackground = Color(0xFF505050)
+val InputLightBackground = Color(0xFFE5E5E5)
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+val Red = Color(0xFFE53935)
+val Green = Color(0xFF43A047)
+val Orange = Color(0xFFFFA726)
+val Blue = Color(0xFF1E88E5)
--- app/src/main/java/com/example/todolist/ui/theme/Theme.kt
+++ app/src/main/java/com/example/todolist/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.todolist.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -12,32 +11,42 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Blue,
+    secondary = Red,
+    tertiary = Green,
+
+    background = MainDarkBackground,
+    surfaceContainerLow = TaskDarkBackground,
+    surface = HeaderDarkBackground,
+    surfaceContainerHigh = InputDarkBackground,
+    onPrimary = White,
+    onSecondary = White,
+    onTertiary = White,
+    onBackground = White,
+    onSurface = White
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Blue,
+    secondary = Red,
+    tertiary = Green,
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    background = MainLightBackground,
+    surfaceContainerLow = TaskLightBackground,
+    surface = HeaderLightBackground,
+    surfaceContainerHigh = InputLightBackground,
+    onPrimary = White,
+    onSecondary = White,
+    onTertiary = White,
+    onBackground = Black,
+    onSurface = Black
 )
 
 @Composable
-fun TODOLISTTheme(
+fun TicTaskTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
